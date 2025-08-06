cfc520077e2414d88a2be33af117d17aea59a898
Roshan Edachali <roshanedachali@Roshans-MacBook-Pro.local>
2025-08-06 15:04:58 -0500

    Add homepage

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to Roshan's Blog</h1>
+      <ul>
+        <li><Link href="/post/hello-world">Hello World</Link></li>
+      </ul>
+    </main>
+  );
+}
